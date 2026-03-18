017775e25db1061840efdd5a0396b70b53a5ca92
Szabó Marcell <szabo-marcell@mailbox.unideb.hu>
2026-03-18 17:21:37 +0100

    feat: add the getMostLikelyCountry() method in Nationality

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,9 +1,14 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
+import java.util.Comparator;
+import java.util.Optional;
 
 import java.util.List;
 
 public record Nationality(long count, String name, @SerializedName("country") List<Country> countries) {
     public record Country(String countryId, float probability) {}
+    public Optional<Country> getMostLikelyCountry() {
+        return countries.stream().max(Comparator.comparingDouble(Country::probability));
+    }
 }
